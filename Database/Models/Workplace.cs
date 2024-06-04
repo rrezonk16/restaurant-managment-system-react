ef9e2857b383cacc57243acc7fa51c71d19db4cf
Rrezon Krasniqi <rrezonkrasniqi32@gmail.com>
2024-06-04 22:24:46 +0200

    Merge

---
+++ Database/Models/Workplace.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Database.Models
+{
+    public class Workplace: BaseModel
+    {
+        public int UserId { get; set; }
+        public int RestaurantId { get; set; }
+
+        // Navigation properties
+        public Users User { get; set; }
+        public Restaurants Restaurant { get; set; }
+    }
+
+}
